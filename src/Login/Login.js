dd2b1d669aea08d8fd375222bdfb55fc15b9e7ba
Kelly Danger <kellyanndanger@gmail.com>
2021-02-09 13:11:51 -0600

    adding button functions

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -6,22 +6,81 @@ class Login extends Component {
   state = {
     register: true,
     login: false,
+  };
+  toggleReg = () => {
+    console.log('REGISTER');
+  };
+  toggleLogin = () => {
+    console.log('LOGIN');
+  }
+  submit = () => {
+    console.log('SUBMIT');
   }
 
   render() {
     return (
       <>
       {this.state.register &&
-        <div className="loginForm">
+        <div className="loginRegForm">
           <Button
-            className="loginRegActive"
-            variant="primary"
+            className="active"
+            id="registerBtn"
+            variant="secondary"
+            onClick={() => this.toggleReg()}
+            disabled
           >Register</Button>
           <Button
-            className="loginRegInactive"
-            variant="primary"
+            className="inactive"
+            id="loginBtn"
+            variant="secondary"
+            onClick={() => this.toggleLogin()}
           >Login</Button>
           <h2>Sign Up for Free</h2>
+          <div className="inputs">
+          <div>
+            <input
+              className="regFormInput"
+              id="firstName"
+              type="text"
+              placeholder="First Name"
+            >
+            </input>
+            <input
+              className="regFormInput"
+              id="lastName"
+              type="text"
+              placeholder="Last Name"
+            >
+            </input>
+          </div>
+          <div>
+            <input
+              className="regFormInput"
+              id="email"
+              type="text"
+              placeholder="Email Address"
+            >
+            </input>
+          </div>
+          <div>
+            <input
+              className="regFormInput"
+              id="password"
+              type="password"
+              placeholder="Set a Password"
+            >
+            </input>
+          </div>
+          </div>
+          <Button
+            className="goBtn"
+            className="active"
+            id="getStartedBtn"
+            variant="primary"
+            onClick={() => this.submit()}
+          >
+            Get Started
+          </Button>
         </div>
       }
       </>
