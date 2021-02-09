da01a80c2739065acae85e03fcd698d451485699
Kelly Danger <kellyanndanger@gmail.com>
2021-02-09 10:44:21 -0600

    initial commit

---
+++ src/Login/Login.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Login.css';
+
+class Login extends Component {
+
+  render() {
+    return (
+      <div>
+        <p>Form</p>
+      </div>
+    )
+  }
+};
+
+export default Login;
