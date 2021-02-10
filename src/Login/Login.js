35cb241db4f7d04aaeafae1709d1493c1e36357c
Kelly Danger <kellyanndanger@gmail.com>
2021-02-10 14:14:54 -0600

    conditional rendering of required fields

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, FormGroup, Row, Col, FormControl, ButtonGroup } from 'react-bootstrap';
+import { Button, Form, Row, Col, FormControl, ButtonGroup } from 'react-bootstrap';
 import './Login.css';
 
 class Login extends Component {
@@ -10,7 +10,11 @@ class Login extends Component {
     lastName: '',
     email: '',
     password: '',
-    validated: false,
+    fnErr: false,
+    lnErr: false,
+    emailErr: false,
+    passErr: false,
+    formValid: true,
   };
   toggleReg = () => {
     console.log('REGISTER');
@@ -25,24 +29,46 @@ class Login extends Component {
       register: false,
       login: true,
     })
-  }
+  };
   handleChange = (event, param) => {
     this.setState({
       [param]: event.target.value
     })
-  }
-
-  submit = () => {
-    console.log('SUBMIT', this.state);
-  }
-
+  };
+  handleValidation = () => {
+    if(!this.state.firstName) {
+      this.setState({
+        fnErr: true,
+      })
+    }
+    if(!this.state.lastName) {
+      this.setState({
+        lnErr: true,
+      })
+    }
+    if(!this.state.email) {
+      this.setState({
+        emailErr: true,
+      })
+    }
+    if(!this.state.password) {
+      this.setState({
+        passErr: true,
+      })
+    }
+  };
+  submit = (event) => {
+    event.preventDefault();
+    this.handleValidation();
+    console.log('Submit', this.state);
+  };
   render() {
     return (
       <>
-        <FormGroup className="loginRegForm">
+        <Form className="loginRegForm" onSubmit={this.submit} noValidate>
           {/* this is the login/register button row */}
+          {/* conditionally renders buttons to show reg or login as active */}
             <Row>
-            {JSON.stringify(this.state.validated)}
               {this.state.register ?
               <ButtonGroup>
                 <Button
@@ -76,7 +102,7 @@ class Login extends Component {
               }
             </Row>
             {/* end login/register row */}
-            {/* headline row */}
+            {/* headline row conditionally renders to show appropriate greeting for chosen form */}
             {this.state.register ?
               <Row>
                 <h2>Sign Up for Free.</h2>
@@ -90,66 +116,120 @@ class Login extends Component {
             {/* name input row */}
             {this.state.register &&
               <Row>
+                {/* firstName conditionally renders on submit if the field is empty */}
+                {this.state.fnErr ?
                   <FormControl
                     className="regFormInput"
                     id="firstName"
                     type="text"
                     placeholder="First Name"
+                    value={this.state.firstName}
                     required
                     onChange={(event) => this.handleChange(event, "firstName")}
                   >
                   </FormControl>
+                  :
+                  <FormControl
+                    className="regFormInput"
+                    id="firstName"
+                    type="text"
+                    placeholder="First Name"
+                    value={this.state.firstName}
+                    onChange={(event) => this.handleChange(event, "firstName")}
+                  >
+                  </FormControl>
+                }
+                {/* lastName conditionally renders on submit if the field is empty */}
+                {this.state.lnErr ?
                   <FormControl
                     className="regFormInput"
                     id="lastName"
                     type="text"
                     placeholder="Last Name"
+                    value={this.state.lastName}
                     required
                     onChange={(event) => this.handleChange(event, "lastName")}
                   >
                   </FormControl>
+                  :
+                  <FormControl
+                    className="regFormInput"
+                    id="lastName"
+                    type="text"
+                    placeholder="Last Name"
+                    value={this.state.lastName}
+                    onChange={(event) => this.handleChange(event, "lastName")}
+                  >
+                  </FormControl>
+                
+                }
               </Row>
             }
             {/* end name input row */}
-            {/* email input row */}
+            {/* email input row conditionally renders on submit if field is empty */}
             <Row> 
               <Col>
-                  <FormControl
-                    className="regFormInput"
-                    id="email"
-                    type="text"
-                    placeholder="Email Address"
-                    required
-                    onChange={(event) => this.handleChange(event, "email")}
-                  >
-                  </FormControl>
+              {this.state.emailErr ?
+                <FormControl
+                  className="regFormInput"
+                  id="email"
+                  type="email"
+                  placeholder="Email Address"
+                  value={this.state.email}
+                  required
+                  onChange={(event) => this.handleChange(event, "email")}
+                >
+                </FormControl>
+                :
+                <FormControl
+                  className="regFormInput"
+                  id="email"
+                  type="email"
+                  placeholder="Email Address"
+                  value={this.state.email}
+                  onChange={(event) => this.handleChange(event, "email")}
+                >
+                </FormControl>
+              }
               </Col>
             </Row>
             {/* end email input row */}
-            {/* password input row */}
+            {/* password input row  conditionally renders on submit if field is empty*/}
             <Row>
-              <Col sm>
-                  <FormControl
-                    className="regFormInput"
-                    id="password"
-                    type="password"
-                    placeholder="Set a Password"
-                    required
-                    onChange={(event) => this.handleChange(event, "password")}
-                  >
-                  </FormControl>
+              <Col>
+              {this.state.passErr ?
+                <FormControl
+                  className="regFormInput"
+                  id="password"
+                  type="password"
+                  placeholder="Set a Password"
+                  value={this.state.password}
+                  required
+                  onChange={(event) => this.handleChange(event, "password")}
+                >
+                </FormControl>
+                :
+                <FormControl
+                className="regFormInput"
+                id="password"
+                type="password"
+                placeholder="Set a Password"
+                value={this.state.password}
+                onChange={(event) => this.handleChange(event, "password")}
+              >
+              </FormControl>
+              }
               </Col>
             </Row>
             {/* end password input row */}
-            {/* go button row */}
+            {/* go button row conditionally renders for each form */}
             <Row>
               <Col sm>
               {this.state.register ?
                 <Button
                   id="getStartedBtn"
                   variant="primary"
-                  // type="submit"
-                  onClick={() => this.submit()}
+                  type="submit"
                 >
                   GET STARTED
                 </Button>
@@ -157,8 +237,7 @@ class Login extends Component {
                 <Button
                   id="getStartedBtn"
                   variant="primary"
-                  // type="submit"
-                  onClick={() => this.submit()}
+                  type="submit"
                 >
                   LOG IN
                 </Button>
@@ -166,7 +245,7 @@ class Login extends Component {
               </Col>
             </Row>
             {/* end go button row */}
-        </FormGroup>
+        </Form>
       </>
     )
   }
